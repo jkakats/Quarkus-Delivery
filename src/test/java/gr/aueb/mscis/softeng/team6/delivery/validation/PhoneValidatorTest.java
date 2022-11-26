fa6880265abaa73a2a7813c797728c5aea0aa571
Ioannis Somos <i.somos@aueb.gr>
2022-11-26 17:42:52 +0200

    refactor: use parameterized tests

--- src/test/java/gr/aueb/mscis/softeng/team6/delivery/validation/PhoneValidatorTest.java
+++ src/test/java/gr/aueb/mscis/softeng/team6/delivery/validation/PhoneValidatorTest.java
@@ -1,16 +1,13 @@
 package gr.aueb.mscis.softeng.team6.delivery.validation;
 
-import org.assertj.core.api.SoftAssertions;
-import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
-import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@ExtendWith(SoftAssertionsExtension.class)
 class PhoneValidatorTest {
-  @InjectSoftAssertions private SoftAssertions softly;
-
   private PhoneValidator validator;
 
   @BeforeEach
@@ -19,25 +16,34 @@ class PhoneValidatorTest {
     validator.initialize(null);
   }
 
-  @Test
-  void testValidateMobile() {
-    softly.assertThat(validator.isValid("6946006665", null)).isTrue();
-    softly.assertThat(validator.isValid("+30 6946006665", null)).isTrue();
-    softly.assertThat(validator.isValid("94278853", null)).isFalse();
-    softly.assertThat(validator.isValid("+357 94278853", null)).isFalse();
+  @ParameterizedTest
+  @ValueSource(strings = {"6946006665", "+30 6946006665"})
+  void testValidMobile(String number) {
+    assertThat(validator.isValid(number, null)).isTrue();
   }
 
-  @Test
-  void testValidateLandline() {
-    softly.assertThat(validator.isValid("2106801900", null)).isTrue();
-    softly.assertThat(validator.isValid("+30 2106801900", null)).isTrue();
-    softly.assertThat(validator.isValid("22628188", null)).isFalse();
-    softly.assertThat(validator.isValid("+357 22628188", null)).isFalse();
+  @ParameterizedTest
+  @ValueSource(strings = {"94278853", "+357 94278853"})
+  void testInvalidMobile(String number) {
+    assertThat(validator.isValid(number, null)).isFalse();
   }
 
-  @Test
-  void testValidateNaN() {
-    softly.assertThat(validator.isValid("", null)).isFalse();
-    softly.assertThat(validator.isValid("NaN", null)).isFalse();
+  @ParameterizedTest
+  @ValueSource(strings = {"2106801900", "+30 2106801900"})
+  void testValidLandline(String number) {
+    assertThat(validator.isValid(number, null)).isTrue();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"22628188", "+357 22628188"})
+  void testInvalidLandline(String number) {
+    assertThat(validator.isValid(number, null)).isFalse();
+  }
+
+  @ParameterizedTest
+  @EmptySource
+  @ValueSource(strings = {"NaN"})
+  void testInvalidNumber(String number) {
+    assertThat(validator.isValid(number, null)).isFalse();
   }
 }
